6f4d7fb07df066febe89d2492f5a56fea278b690
DaniArellano0348 <160549791+DaniArellano0348@users.noreply.github.com>
2024-03-05 00:20:25 +0000

    Version 5

--- Alimento.hpp
+++ Alimento.hpp
@@ -11,3 +11,4 @@ class Alimento
                 return this -> Energia;
             }
            }
+           
--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include <Mascota.hpp>
-using namespace std; 
+#include <Alimento.hpp>
+#include <list>
+
 int main(int argc, char const *argv[])
 {
-Mascota m1;
-Mascota m2;
+    std::list<Mascota> mascotas;
+    Mascota m1("Firulais");
+    Mascota m2("Coraje");
+    mascotas.push_back(m1);
+    mascotas.push_back(m2);
+    Alimento a1(5);
+    Alimento a2(2);
 
-m1.Comer();
-m2.Comer();
+    m1.Comer(a1);
+    m2.Comer(a2);
+    for (auto &&mascota : mascotas)
+    {
+        std::cout
+            << mascota.LeerNombre()
+            <<":"
+            <<mascota.LeerEnergia()
+            << std::endl;
+    }
 
-    cout <<"Mascota 1"<<m1.LeerEneegia()<< endl;
-    <<"Mascota 2 Energia"<<m2.LeerEnergia()<< endl;
     return 0;
 }
